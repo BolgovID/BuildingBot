39463be3334a92dd0ba8aad10cfcf8927e2eaf45
BolgovID <47144316+BolgovID@users.noreply.github.com>
2019-05-29 17:06:31 +0300

    213414

--- src/main/java/Settings.java
+++ src/main/java/Settings.java
@@ -16,6 +16,6 @@ public class Settings {
 
     public static final String BotLink = "https://t.me/share/url?url=http%3A//t.me/BuildingLinkBot&text=%D0%A1%D1%82%D1%80%D0%BE%D0%B8%D1%82%D0%B5%D0%BB%D1%8C%D0%BD%D1%8B%D0%B9%20%D0%B1%D0%BE%D1%82";
     public static final String ChannelLink = "https://t.me/StroyObzorKh";
-    public static final String ExcelLink="./Repository/Houses.xlsx";
+    public static final String ExcelLink="src/main/java/Houses.xlsx";
 
 }
